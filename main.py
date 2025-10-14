cad022b203adc465dd459a279005e2dc8d0ebd94
Rubén Alava <ruben777_93@hotmail.com>
2025-10-14 10:45:24 -0500

    se refactoriza el codigo y se formatea mas bonito jeje

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import argparse
 from src.use_cases.register_face import register_face
 from src.use_cases.recognize_face import recognize_face
 
+
 def main():
     parser = argparse.ArgumentParser(description="Face Recognition CLI")
     subparsers = parser.add_subparsers(dest="command", help="Comando a ejecutar")
 
-    register_parser = subparsers.add_parser("register", help="Registrar un nuevo rostro")
+    register_parser = subparsers.add_parser(
+        "register", help="Registrar un nuevo rostro"
+    )
     register_parser.add_argument("--name", required=True, help="Nombre de la persona")
 
-    recognize_parser = subparsers.add_parser("recognize", help="Reconocer un rostro desde la cámara")
-    recognize_parser.add_argument("--image", help="Ruta a una imagen (opcional). Si no, usa la cámara.")
+    recognize_parser = subparsers.add_parser(
+        "recognize", help="Reconocer un rostro desde la cámara"
+    )
+    recognize_parser.add_argument(
+        "--image", help="Ruta a una imagen (opcional). Si no, usa la cámara."
+    )
 
     args = parser.parse_args()
 
@@ -20,6 +27,7 @@ def main():
         recognize_face(args.image)
     else:
         parser.print_help()
+
 
 if __name__ == "__main__":
     main()
